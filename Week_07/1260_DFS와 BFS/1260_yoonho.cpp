bca519f17254005b12d162feb5857ab25e2a4841
hnn.ynh <83288181+hnnynh@users.noreply.github.com>
2022-05-18 01:57:42 +0900

    7주차 제출

---
+++ Week_07/1260_DFS와 BFS/1260_yoonho.cpp
@@ -0,0 +1,59 @@
+﻿#include<iostream>
+#include<queue>
+
+using namespace std;
+
+int graph[1001][1001];
+bool visited[1001];
+int N, M;
+
+void DFS(int v) {
+	visited[v] = true;
+	cout << v << " ";
+
+	for (int i = 1; i <= N; i++) {
+		if (graph[v][i] == 1 && !visited[i]) {
+			DFS(i);
+		}
+	}
+}
+
+void BFS(int v) {
+	queue<int> q;
+	q.push(v);
+	visited[v] = true;
+	cout << v << " ";
+
+	while (!q.empty()) {
+		v = q.front();
+		q.pop();
+		
+		for (int i = 1; i <= N; i++) {
+			if (graph[v][i] == 1 && !visited[i]) { //현재 정점과 연결되어있고 방문되지 않았으면
+				q.push(i);
+				visited[i] = true;
+				cout << i << " ";
+			}
+		}
+	}
+}
+
+int main() {
+	
+	int V, c1, c2;
+	cin >> N >> M >> V;
+	for (int i = 0; i < M; i++) {
+		cin >> c1 >> c2;
+		graph[c1][c2] = 1;
+		graph[c2][c1] = 1;
+	}
+
+	DFS(V);
+	for (int i = 1; i <= N; i++) {
+		visited[i] = false;
+	}
+	cout << '\n';
+	BFS(V);
+
+	return 0;
+}
---
+++ Week_07/7576_토마토/7576_yoonho.cpp
@@ -0,0 +1,61 @@
+﻿#include<iostream>
+#include<queue>
+
+using namespace std;
+
+int dx[] = { 0,0,1,-1 };
+int dy[] = { 1,-1,0,0 };
+int tomato[1000][1000];
+int N, M;
+queue<pair<int, int>> q;
+
+void BFS() {
+
+	while (!q.empty()) {
+		int x = q.front().first;
+		int y = q.front().second;
+		q.pop();
+
+		for (int i = 0; i < 4; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+			if (0 <= nx && 0 <= ny && nx < N && ny < M) {
+				if (tomato[nx][ny] == 0) {
+					tomato[nx][ny] = tomato[x][y] + 1;
+					q.push({ nx, ny });
+				}
+			}
+		}
+	}
+}
+
+int main() {
+	
+	int t, day = 0;
+	cin >> M >> N;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			cin >> t;
+			tomato[i][j] = t;
+			if (tomato[i][j] == 1)
+				q.push({ i,j });
+		}
+	}
+
+	BFS();
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			if (tomato[i][j] > day)
+				day = tomato[i][j];
+			if (tomato[i][j] == 0) {
+				cout << -1;
+				return 0;
+			}
+		}
+	}
+
+	cout << day - 1;
+
+	return 0;
+}
