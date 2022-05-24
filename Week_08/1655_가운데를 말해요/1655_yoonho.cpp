59189aca44bf8b150b792ddadbb286825e47bf66
Yoonho Hann <83288181+hnnynh@users.noreply.github.com>
2022-05-25 00:25:26 +0900

    우선순위큐 윤호 제출

---
+++ Week_08/1655_가운데를 말해요/1655_yoonho.cpp
@@ -0,0 +1,40 @@
+﻿// 반을 나눠 작은 것들의 최댓값 -> 중간값
+
+#include<iostream>
+#include<queue>
+
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(0);
+
+	priority_queue<int> maxH, minH;
+	vector<int> v;
+
+	int N, x, temp1, temp2;
+	cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		cin >> x;
+		if (maxH.size() > minH.size())
+			minH.push(-x);
+		else
+			maxH.push(x);
+
+		if (!minH.empty() && maxH.top() > -minH.top()) {
+			temp1 = maxH.top();
+			temp2 = -minH.top();
+			maxH.pop();
+			minH.pop();
+
+			maxH.push(temp2);
+			minH.push(-temp1);
+		}
+
+		cout << maxH.top() << '\n';
+	}
+
+	return 0;
+}
